e2867f54670450960f0523f5ea554ad35c190bc3
Rens <renskrauweel@hotmail.com>
2018-10-03 17:34:19 +0200

    Robot grabbing task

---
+++ AmazonSimulator VS/Models/RobotGrab.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class RobotGrab : IRobotTask
+    {
+        private Suitcase suitcase;
+        private char pickupNode;
+        private List<Coordinate> coordinates;
+        private Graph g;
+        private Coordinate home = new Coordinate(15, 0, 5);
+        private bool walkingHome = false;
+
+        public RobotGrab(char pickupNode, Suitcase suitcase, List<Coordinate> coordinates, Graph g)
+        {
+            this.pickupNode = pickupNode;
+            this.suitcase = suitcase;
+            this.coordinates = coordinates;
+            this.g = g;
+        }
+
+        public void StartTask(Robot r)
+        {
+            suitcase.x = r.x;
+            suitcase.y = r.y + 0.3;
+            suitcase.z = r.z;
+
+            r.SetSuitcase(suitcase);
+
+            if (!walkingHome)
+            {
+                r.AddTask(new RobotMove(g.shortest_path(pickupNode, 'A'), coordinates, g));
+                r.AddTask(new RobotRelease(suitcase, home));
+                walkingHome = true;
+            }
+        }
+
+        public bool TaskComplete(Robot r)
+        {
+            bool complete = (suitcase.x == r.x && suitcase.y == r.y + 0.3 && suitcase.z == r.z);
+            if (complete)
+            {
+                walkingHome = false;
+                suitcase.x = home.GetX();
+                suitcase.y = home.GetY();
+                suitcase.z = home.GetZ();
+            }
+            return complete;
+        }
+    }
+}
