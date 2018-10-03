24e97c05e6304ad3c8b862946081c4a6514893b5
dantheman0125 <danielroskam@gmail.com>
2018-10-03 19:33:24 +0200

    new class airplaneMove

---
+++ AmazonSimulator VS/Models/AirplaneMove.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AirplaneMove : ITask
+    {
+        List<Coordinate> coordinates;
+
+        public void StartTask(BaseModel r)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TaskComplete(BaseModel r)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
